e05827ddb1b771a05d59c708340d37f001d6be68
Rachael Judy <rljudy74981@icloud.com>
2021-08-24 17:26:53 -0400

    update cpu_core

--- cpu_core.vhd
+++ cpu_core.vhd
@@ -5,8 +5,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
  
- entity cpu_core is
-  port ( 
+entity cpu_core is
+port ( 
 	I_CLK : in std_logic;
 	I_CLR : in std_logic;
 	I_INTVEC : in std_logic_vector( 5 downto 0);
@@ -19,4 +19,16 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 	Q_RD_IO : out std_logic;
 	Q_WE_IO : out std_logic
 	
-	);
+);
+end cpu_core;
+
+architecture behavior of cpu_core is
+
+
+begin
+
+
+  L_DIN <= F_PM_DOUT when (D_PMS = '1') else I_DIN(7 downto 0);
+  L_INTVEC_5 <= I_INTVEC(5) and R_INT_ENA;
+
+end behavior;
