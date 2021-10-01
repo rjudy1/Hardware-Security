562b1d8430d741b67112302ce0495ec7a651e468
Rachael Judy <rljudy74981@icloud.com>
2021-10-01 08:11:30 -0400

    uart

--- src/uart.vhd
+++ src/uart.vhd
@@ -23,7 +23,7 @@ end uart;
 
 architecture Behavioral of uart_rx is
 	 
- entity uart_rx is
+component uart_rx
     PORT(   I_CLK       : in  std_logic;
             I_CLR       : in  std_logic;
             I_CE_16     : in  std_logic;            -- 16 times baud rate 
@@ -31,8 +31,9 @@ architecture Behavioral of uart_rx is
  
             Q_DATA      : out std_logic_vector(7 downto 0);
             Q_FLAG      : out std_logic);       -- toggle on every byte received
-end uart_rx;
- entity uart_tx is 
+end component;
+
+component uart_tx
      port(   I_CLK       : in  std_logic;    
              I_CLR       : in  std_logic;            -- RESET
              I_CE_1      : in  std_logic;            -- BAUD rate clock enable
@@ -41,9 +42,29 @@ end uart_rx;
              Q_TX        : out std_logic;            -- Serial output line
              Q_FLAG      : out std_logic);           -- Transmitting Flag
  end uart_tx;
- 	 
+ 
+ signal high : std_logic;
 	 
 begin
-	 
+	high <= '1';
+	tx : uart_tx
+    port(   I_CLK       => I_CLK,    
+            I_CLR       => I_CLR,            -- RESET
+            I_CE_1      => high,            -- BAUD rate clock enable
+            I_DATA      => I_TX_DATA,   -- DATA to be sent
+            I_FLAG      => I_WE,            -- toggle to send data
+            Q_TX        => Q_TX_DATA,            -- Serial output line
+            Q_FLAG      => Q_TX_BUSY             -- Transmitting Flag
+	);
+
+	rx : uart_rx
+    PORT(   I_CLK       => I_CLK,
+            I_CLR       => I_CLR,
+            I_CE_16     => I_RD,           -- 16 times baud rate 
+            I_RX        => I_RX,           -- Serial input line
+ 
+            Q_DATA      => Q_RX_DATA,
+            Q_FLAG      => Q_RX_READY       -- toggle on every byte received
+	);
 
 end Behavioral;
