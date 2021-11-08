3a1790c10b459fdeba0ce9611b13bfbbf4fc0bbe
Rachael Judy <64846855+rjudy1@users.noreply.github.com>
2021-11-07 19:54:57 -0500

    Update the basic button and seven seg test

--- test/C_Programs/AddPortTest/AddPortTest/main.c
+++ test/C_Programs/AddPortTest/AddPortTest/main.c
@@ -15,15 +15,17 @@
 int main(void)
 {
 	DDRB = 0b11111111;					//PORTB is set to write (set as output)
-	DDRC = 0b00000000;					// PORTB is set as input
+	DDRC = 0b11110000;					// PORTc top is sel is set as input
+	DDRD = 0b00000000; //PORT D is buttons
 	int go = 0;
 	while(1)
 	{
-		if (PORTC != 0b00000000)
+		if (PORTD != 0b00000000)
 		{
 			go = 1;
 		}
 		if (go) {
+			PORTC - 0b11000000;				// flash top two on and off
 			PORTB = 0b00000000;				//PORTB is all low
 			_delay_ms(40);					//Pause for 40 ms
 			PORTB = 0b11111111;				//PORTB is all high
