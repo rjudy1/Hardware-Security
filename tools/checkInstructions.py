b38889a7f14c183867e343cd619298f81643756b
Andrew Smith <andrewsmith@cedarville.edu>
2021-11-09 09:48:32 -0500

    Added check for invalid registers

--- tools/checkInstructions.py
+++ tools/checkInstructions.py
@@ -1,6 +1,6 @@
 import sys
 validInstruction = ["ADC", "ADD", "ADIW", "AND", "ANDI", "ASR", "BCLR", "BLD", "BRBS", "BREQ", "BRCS", "BRBC", "BRNE", "BRCC", "BSET", "BST", "CALL", "CBI", "CBR", "CLR", "COM", "CP", "CPC", "CPI", "CPSE", "DEC", "ELPM", "EOR", "FMUL", "FMULS", "FMULSU", "ICALL", "IN", "INC", "IJMP", "JMP", "LDD", "LDI", "LDS", "LPM", "LSL", "LSR", "MOV", "MOVW", "MULSU", "MULS", "NEG", "NOP", "NOT", "OR", "ORI", "OUT", "POP", "PUSH", "RCALL", "RET", "RETI", "ROR", "RJMP", "ROL", "SBC", "SBCI", "SBI", "SBIC", "SBIS", "SBIW", "SBRC", "SBRS", "SDS", "SDD", "SER", "SPM", "STD", "STS", "SUB", "SUBI", "SWAP", "WDR"]
-
+invalidRange = ["0x22", "0x23", "0x24", "0x25", "0x26", "0x27", "0x28", "0x29", "0x2D", "0x2E", "0x2F", "0x30", "0x31", "0x32", "0x33", "0x34", "0x35", "0x36", "0x37", "0x38", "0x39", "0x3A", "0x3B", "0x3C", "0x3D", "0x3E", "0x3F", "0x40", "0x41", "0x42", "0x43", "0x44", "0x45", "0x46", "0x47", "0x48", "0x49", "0x4A", "0x4B", "0x4C", "0x4D", "0x4E", "0x4F", "0x50", "0x51", "0x52", "0x53", "0x54", "0x55", "0x56", "0x57", "0x58", "0x59", "0x5A", "0x5B", "0x5C", "0x5D", "0x5E"]
 
 #Functions
 def waitForInput():
@@ -34,6 +34,8 @@ Lines = file.readlines()
 seenInstructions = []
 invalidInstruction = []
 
+print("These instructions may have invalid registers:")
+
 for line in Lines:
     if line[0].lower() == " ":          #only lines with spaces have instructions
         tokens = line.split()
@@ -42,6 +44,11 @@ for line in Lines:
                 seenInstructions.append(tokens[3].upper())
                 if(tokens[3].upper() not in validInstruction):
                     invalidInstruction.append(tokens[3].upper())
+
+            if any(s in line for s in invalidRange):
+                print(line)
+
+
 
 invalidInstruction.sort()
 
