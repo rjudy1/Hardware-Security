f0c8c277aea6d54c0eda89183d9ce240029e152c
Rachael Judy <rjudy@cedarville.edu>
2021-11-12 14:52:43 -0500

    replace uart and add instructions for tx

--- src/uart_tx.vhd
+++ src/uart_tx.vhd
@@ -1,76 +1,56 @@
- ------------------------------------------------------------------------------
- --
- -- Module Name:    uart_tx - Behavioral 
- -- Create Date:    14:21:59 11/07/2009 
- -- Description:    a UART receiver.
- --
- -------------------------------------------------------------------------------
- --
- library IEEE;
- use IEEE.STD_LOGIC_1164.ALL;
- use IEEE.STD_LOGIC_ARITH.ALL;
- use IEEE.STD_LOGIC_UNSIGNED.ALL;
+-- Module Name:    uart_tx - Behavioral 
+-- Create Date:    14:21:59 11/07/2009 
+-- Description:    a UART receiver.
+--
+-------------------------------------------------------------------------------
+--
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity uart_tx is 
+    port(   I_CLK       : in  std_logic;    
+            I_CLR       : in  std_logic;            -- RESET
+            I_CE_1      : in  std_logic;            -- BAUD rate clock enable
+            I_DATA      : in  std_logic_vector(7 downto 0);   -- DATA to send
+            I_FLAG      : in  std_logic;            -- toggle to send data
+            Q_TX_N      : out std_logic;            -- Serial output, active low
+            Q_BUSY      : out std_logic);           -- Transmitter busy
+end uart_tx;
  
- entity uart_tx is 
-     port(   I_CLK       : in  std_logic;    
-             I_CLR       : in  std_logic;            -- RESET
-             I_CE_1      : in  std_logic;            -- BAUD rate clock enable
-             I_DATA      : in  std_logic_vector(7 downto 0);   -- DATA to be sent
-             I_FLAG      : in  std_logic;            -- toggle to send data
-             Q_TX        : out std_logic;            -- Serial output line
-             Q_FLAG      : out std_logic);           -- Transmitting Flag
- end uart_tx;
-     
- architecture Behavioral of uart_tx is
-  
-    signal L_BUF            : std_logic_vector(7 downto 0);
-    signal L_TODO           : std_logic_vector(3 downto 0);     -- bits to send
-    signal L_FLAG           : std_logic := '0';
-  
-  attribute mark_debug : string;
-  attribute mark_debug of Q_TX : signal is "true";
-  attribute mark_debug of L_TODO : signal is "true";
-  attribute mark_debug of L_FLAG : signal is "true";
-  attribute mark_debug of I_FLAG : signal is "true";
-  attribute mark_debug of L_BUF : signal is "true";
-  attribute mark_debug of I_CLK : signal is "true";
+architecture Behavioral of uart_tx is
  
-    
- begin
-     
+signal L_BUF            : std_logic_vector(8 downto 0);
+signal L_FLAG           : std_logic;
+signal L_TODO           : std_logic_vector(3 downto 0);     -- bits to send
+ 
+begin
+ 
     process(I_CLK)
-        begin
-            if (rising_edge(I_CLK)) then
-                if (I_CLR = '1') then
-                    Q_TX   <= '1';
-                    L_BUF  <= "11111111";
-                    L_TODO <= "0000";
-                    L_FLAG <= I_FLAG;                   -- idle
-                elsif (I_CE_1 = '1') then
-                   if (L_TODO = "1001") then
-                        Q_TX <= '0';
-                        L_TODO <= L_TODO - "0001";
-                   elsif (L_TODO /= "1111") then          -- transmitting
-                        Q_TX <= L_BUF(0);               -- next bit
-                        L_BUF     <= '1' & L_BUF(7 downto 1);
-                        if (L_TODO = "0001") then
-                            L_FLAG <= I_FLAG;
-                        end if;
-                        L_TODO <= L_TODO - "0001";
-                   end if;
---                   L_TODO <= L_TODO - "0001";
-                 end if;
-                 
-                 -- possibly an issue with this being outside i_ce_1?
-                 -- might require a sufficient delay to work
-                 if (L_FLAG /= I_FLAG) then           -- new byte
---                        Q_TX <= '0';                -- start bit
-                      L_BUF <= NOT(I_DATA);                -- data bits
-                      L_TODO <= "1001";
-                 end if;
+    begin
+        if (rising_edge(I_CLK)) then
+            if (I_CLR = '1') then               -- reset
+                Q_TX_N <= '1';
+                L_BUF  <= "111111111";
+                L_TODO <= "0000";
+                L_FLAG <= I_FLAG;
+            elsif (L_TODO = "0000") then        -- idle
+                if (L_FLAG /= I_FLAG) then      -- new byte
+                    L_BUF <= I_DATA & '0';      -- 8 data / 1 start
+                    L_TODO <= "1100";           -- 11 bits to send
+                    L_FLAG <= I_FLAG;
+                end if;
+            else                                -- shifting
+                if (I_CE_1 = '1') then 
+                    Q_TX_N <= L_BUF(0);
+                    L_BUF <= '1' & L_BUF(8 downto 1);
+                    L_TODO <= L_TODO - "0001";
+                end if;
             end if;
-        end process; 
-  
-     Q_FLAG <= L_FLAG;
-     
- end Behavioral;  
+        end if;
+    end process; 
+ 
+    Q_BUSY <= '0' when (L_TODO = "0000") else '1';
+ 
+end Behavioral;  
